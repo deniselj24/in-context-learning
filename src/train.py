a4c97b6da50d8b3253d46c6c59c85824c6c549fa
deniselj24 <deniseleehjsg@gmail.com>
2025-01-16 16:09:40 -0800

    Update train.py

--- src/train.py
+++ src/train.py
@@ -138,7 +138,7 @@ def train(model, args):
 
         # Log hessian every 1000 steps 
         train_data = (xs, ys)
-        if i % 2000 == 0: 
+        if i % 5000 == 0: 
             plot_hessian(model, train_data, i)
 
         loss_func = task.get_training_metric()
