cb7d95670e418da93509ead75705c3da9dcb2f3e
deniselj24 <deniseleehjsg@gmail.com>
2025-01-22 17:03:01 -0800

    Update entropy.py

--- src/entropy.py
+++ src/entropy.py
@@ -11,6 +11,7 @@ def filter_eigenvalues(eigen_list, weight_list, threshold=None):
     filtered_eigen = []
     filtered_weight = []
     #print(np.max(weight_list))
+    print("Debugging filter eigenvalues", np.array(eigen_list).shape, np.array(weight_list).shape)
     for eig, w in zip(eigen_list, weight_list):
         if threshold is not None:
             if eig >= threshold and w >= 1e-7:
@@ -31,26 +32,19 @@ def gaussian_density(t, values, sigma=1e-5**0.5):
     density = coeff * val
     return density
 
-def interpolate(weights, values):
-    left_boundary = np.mean(np.min(values, axis = 1))-1
-    right_boundary= np.mean(np.max(values, axis = 1)) +1
-    n_grid = 50000
-    grid = np.linspace(left_boundary, right_boundary, n_grid).tolist()
-    density_all = np.zeros((10, n_grid))
-    weights = np.array(weights)
-    values = np.array(values)
+def get_spectral_density(weights, values, n_lanczos=10):
+    #weights = np.array(weights)
+    #values = np.array(values)
+    density_all = np.zeros((n_lanczos, values.shape[1]))
 
-    for k in range(10):
-        for idx, t in enumerate(grid):
+    for k in range(n_lanczos):
+        for idx, t in enumerate(values):
             values_each_v_t = gaussian_density(t, values[k,:])
             density_each_v_t = np.sum(values_each_v_t * weights[k,:])
             density_all[k,idx] = density_each_v_t
-
-    density_avg = np.nanmean(density_all, axis = 0)
-    norm_fact = np.sum(density_avg)*(grid[1]- grid[0])
-    density_avg /= norm_fact
-
-    return grid, density_avg
+    # average across lanczos 
+    # density_avg = np.nanmean(density_all, axis = 0)
+    return density_all
 
 def renormalize_weights(filtered_weight, epsilon=1e-12):
     total = sum(filtered_weight)
@@ -62,28 +56,38 @@ def renormalize_weights(filtered_weight, epsilon=1e-12):
     return renormalized_weight
 
 def compute_spectral_entropy(values_dic, weights_dic, epsilon=1e-12):
+    spectral_entropy = {}
+    weighted_entropy = {}
+    centroid = {}
+    spread = {}
 
     for name in weights_dic.keys(): 
-        weights = weights_dic[name]
-        values = values_dic[name]
+        weights = np.array(weights_dic[name])
+        values = np.array(values_dic[name])
         #print("weights", weights)
         #print("values", values)
-        grid, density_avg = interpolate(weights, values)
+        density = get_spectral_density(weights, values)
+        # renormalize density which is a np array of (n_lanczos=10, 100)
+        total = np.sum(density, axis=1).reshape((10, 1))
+        normalized_density = density / total
 
-    print("density", len(density_avg))
-    print("grid", len(grid))
+        # filtered_eigen, filtered_weight = filter_eigenvalues(values, density)
+        # renormalized_weight = renormalize_weights(filtered_weight)
+        # p = np.array(renormalized_weight) + epsilon  # Avoid log(0)
+        # filtered_eigen = values
+        # renormalized_weight = density
+        p = np.array(normalized_density) + epsilon
+        print(p.shape, np.array(values).shape)
+        spectral_entropy[name] = -np.sum(np.mean(p * np.log(p), axis=0))
+        #print("log", np.log(p))
+        #print("product", p * np.log(p))
+        #print("mean", np.mean(p * np.log(p), axis=0))
+        print(spectral_entropy[name])
+        # print("p min", min(p), "p max", max(p), "eigenvalue min", min(filtered_eigen), "eigenvalue max", max(filtered_eigen))
+        weighted_entropy[name] = -np.sum(p * np.log(p) * np.array(values))
+        centroid[name] = np.sum(np.array(normalized_density) * np.array(values))
+        spread[name] = np.sum(np.array(normalized_density) * (np.array(values) - centroid[name])**2)
 
-    filtered_eigen, filtered_weight = filter_eigenvalues(grid, density_avg)
-    # filtered_eigen, renormalized_weight = grid, density_avg
-    renormalized_weight = renormalize_weights(filtered_weight)
-    p = np.array(renormalized_weight) #+ epsilon  # Avoid log(0)
-    print(len(p), len(filtered_eigen))
-    spectral_entropy = -np.sum(p * np.log(p))
-    print("p min", min(p), "p max", max(p), "eigenvalue min", min(filtered_eigen), "eigenvalue max", max(filtered_eigen))
-    weighted_entropy = -np.sum(p * np.log(p) * np.array(filtered_eigen))
-    centroid = np.sum(np.array(renormalized_weight) * np.array(filtered_eigen))
-    spread = np.sum(np.array(renormalized_weight) * (np.array(filtered_eigen) - centroid)**2)
-        
     return spectral_entropy, weighted_entropy, centroid, spread
 
 
@@ -115,76 +119,78 @@ if __name__ == "__main__":
                 if n not in entropy[key]:
                     entropy[key][n] = {}
                 entropy[key][n][ckpt] = eval(key)
-
+                # spectral_entropy, etc. is dictionary with layer name as key 
+    print(entropy)
     with open(os.path.join(data_dir, "entropy.json"), "w") as f:
         json.dump(entropy, f)
 
-    # plot weighted entropy
-    for i in args.n:
-        n = 2 ** i
-        iterations = sorted(entropy["weighted_entropy"][n].keys())
-        entropies = [entropy["weighted_entropy"][n][i] for i in iterations]
-        plt.plot(iterations, entropies, 
-                label=f"{n} Tasks", 
-                marker='o',
-                markersize=4,
-                linewidth=2)
+    for layer_name in entropy["weighted_entropy"][2][0].keys():
+        # plot weighted entropy
+        for i in args.n:
+            n = 2 ** i
+            iterations = sorted(entropy["weighted_entropy"][n].keys())
+            entropies = [entropy["weighted_entropy"][n][j][layer_name] for j in iterations]
+            plt.plot(iterations, entropies, 
+                    label=f"{n} Tasks", 
+                    marker='o',
+                    markersize=4,
+                    linewidth=2)
 
-    plt.xlabel("Number of training iterations")
-    plt.ylabel("Weighted Entropy")
-    plt.legend()
-    plt.savefig(os.path.join(data_dir, f"weighted_entropy.png"))
-    plt.close()
+        plt.xlabel("Number of training iterations")
+        plt.ylabel("Weighted Entropy")
+        plt.legend()
+        plt.savefig(os.path.join(data_dir, f"weighted_entropy_{layer_name}.png"))
+        plt.close()
 
-    # plot spectral entropy
-    for i in args.n:
-        n = 2 ** i
-        iterations = sorted(entropy["spectral_entropy"][n].keys())
-        entropies = [entropy["spectral_entropy"][n][i] for i in iterations]
-        plt.plot(iterations, entropies, 
-                label=f"{n} Tasks", 
-                marker='o',
-                markersize=4,
-                linewidth=2)
+        # plot spectral entropy
+        for i in args.n:
+            n = 2 ** i
+            iterations = sorted(entropy["spectral_entropy"][n].keys())
+            entropies = [entropy["spectral_entropy"][n][j][layer_name] for j in iterations]
+            plt.plot(iterations, entropies, 
+                    label=f"{n} Tasks", 
+                    marker='o',
+                    markersize=4,
+                    linewidth=2)
 
-    plt.xlabel("Number of training iterations")
-    plt.ylabel("Spectral Entropy")
-    plt.legend()
-    plt.savefig(os.path.join(data_dir, f"spectral_entropy.png"))
-    plt.close()
+        plt.xlabel("Number of training iterations")
+        plt.ylabel("Spectral Entropy")
+        plt.legend()
+        plt.savefig(os.path.join(data_dir, f"spectral_entropy_{layer_name}.png"))
+        plt.close()
 
-    # plot spread
-    for i in args.n:
-        n = 2 ** i
-        iterations = sorted(entropy["spread"][n].keys())
-        entropies = [entropy["spread"][n][i] for i in iterations]
-        plt.plot(iterations, entropies, 
-                label=f"{n} Tasks", 
-                marker='o',
-                markersize=4,
-                linewidth=2)
+        # plot spread
+        for i in args.n:
+            n = 2 ** i
+            iterations = sorted(entropy["spread"][n].keys())
+            entropies = [entropy["spread"][n][j][layer_name] for j in iterations]
+            plt.plot(iterations, entropies, 
+                    label=f"{n} Tasks", 
+                    marker='o',
+                    markersize=4,
+                    linewidth=2)
 
-    plt.xlabel("Number of training iterations")
-    plt.ylabel("Spread")
-    plt.legend()
-    plt.savefig(os.path.join(data_dir, f"spread.png"))
-    plt.close()
+        plt.xlabel("Number of training iterations")
+        plt.ylabel("Spread")
+        plt.legend()
+        plt.savefig(os.path.join(data_dir, f"spread_{layer_name}.png"))
+        plt.close()
 
-    # plot centroid
-    for i in args.n:
-        n = 2 ** i
-        iterations = sorted(entropy["centroid"][n].keys())
-        entropies = [entropy["centroid"][n][i] for i in iterations]
-        plt.plot(iterations, entropies, 
-                label=f"{n} Tasks", 
-                marker='o',
-                markersize=4,
-                linewidth=2)
+        # plot centroid
+        for i in args.n:
+            n = 2 ** i
+            iterations = sorted(entropy["centroid"][n].keys())
+            entropies = [entropy["centroid"][n][j][layer_name] for j in iterations]
+            plt.plot(iterations, entropies, 
+                    label=f"{n} Tasks", 
+                    marker='o',
+                    markersize=4,
+                    linewidth=2)
 
-    plt.xlabel("Number of training iterations")
-    plt.ylabel("Centroid")
-    plt.legend()
-    plt.savefig(os.path.join(data_dir, f"centroid.png"))
-    plt.close()
+        plt.xlabel("Number of training iterations")
+        plt.ylabel("Centroid")
+        plt.legend()
+        plt.savefig(os.path.join(data_dir, f"centroid_{layer_name}.png"))
+        plt.close()
 
 
